54029adb714d0fd4de90f4e55cd440d674433066
Asim Zahoor <113241389+asimzahoor443@users.noreply.github.com>
2025-03-19 14:30:07 +0500

    update

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,11 +1,10 @@
 import { Index } from '@upstash/vector';
-
 type Product = {
   id: string;
   imageId: string;
   name: string;
   size: 'S' | 'M' | 'L';
-  color: 'white' | 'black' | 'blue' | 'green' | 'purple';
+  color: 'white' | 'beige' | 'blue' | 'green' | 'purple';
   price: number;
 };
 export const db = new Index<Product>();
