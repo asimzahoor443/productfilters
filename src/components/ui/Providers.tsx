847b1a6defc55e7832e18e77abffb242f734a262
Asim Zahoor <113241389+asimzahoor443@users.noreply.github.com>
2025-03-28 16:14:49 +0500

    docs: updates with the few packages

---
+++ src/components/ui/Providers.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import { QueryClientProvider } from '@tanstack/react-query';
+const Providers = () => {
+  return <QueryClientProvider></QueryClientProvider>;
+};
+
+export default Providers;
